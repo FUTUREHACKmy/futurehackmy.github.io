d8387ffbc07b7c2c5826ddda38d9b28da633186b
Ivan Tham <pickfire@riseup.net>
2018-01-20 22:52:05 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+for (el of document.getElementsByClassName('image')) {
+  el.innerHTML = `<svg viewBox="0 0 16 14" class="triangle" style="transform: rotate(${Math.random()*360}deg) scale(${Math.random()*0.2+0.8})"><path d="M8 6L10 9L6 9Z"/></svg>` + el.innerHTML;
+};
