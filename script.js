3e517e54d1fb6779de686d588db75d704ba43820
Ivan Tham <pickfire@riseup.net>
2018-01-24 02:23:01 +0800

    Single page app

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 for (el of document.getElementsByClassName('image')) {
   el.innerHTML = `<svg viewBox="0 0 16 14" class="triangle" style="transform: rotate(${Math.random()*360}deg) scale(${Math.random()*0.2+0.8})"><path d="M8 6L10 9L6 9Z"/></svg>` + el.innerHTML;
 };
+
+window.onscroll = function() {
+  let cl = document.querySelector('header').classList;
+  if (window.scrollY > 50) {
+    cl.add('dark');
+  } else {
+    cl.remove('dark');
+  }
+}
