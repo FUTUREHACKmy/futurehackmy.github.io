11f77bcf7f6da85dfe90aacca6fda8bdf0fd2f8a
Ivan Tham <pickfire@riseup.net>
2018-01-28 00:17:43 +0800

    Fix smooth scrolling

--- js/agency.js
+++ js/agency.js
@@ -1,24 +1,19 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 10)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: 'nav', offset: 50});
 
-//     Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-        target: '#mainNav',
-        offset: 150
+    // Smooth scrolling using jQuery
+    $('nav a:not([href="#"])').on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 700, function() {
+            window.location.hash = hash;
+        });
+        return false;
     });
 
     // Scroll Reveal calls
