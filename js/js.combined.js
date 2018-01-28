f99576fc355b5b1cf6a85a12be7dbd8b5925bf5c
Chee Seng Leong <macintoshleong@gmail.com>
2018-01-28 13:42:26 +0800

    Last few updates

--- js/js.combined.js
+++ js/js.combined.js
@@ -1,5 +1,2 @@
 <!--#include file="agency.js" -->
-<!--#include file="contact_me.js" -->
 <!--#include file="google-maps.js" -->
-<!--#include file="jqBootstrapValidation.js" -->
-<!--#include file="timer.js" -->
